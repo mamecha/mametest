f4d3251d461fa0dad7291e99dec0832b48e37b8a
mamecha <yajima@ans-web.co.jp>
2013-07-26 20:59:51 +0900

    Initial Commit

---
+++ mametest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  mametestAppController.h
+//  mametest
+//
+//  Created by 矢島 新太郎 on 2013/07/26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ mametest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  mametestAppController.h
+//  mametest
+//
+//  Created by 矢島 新太郎 on 2013/07/26.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
